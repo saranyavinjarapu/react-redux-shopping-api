596c8361a974f6e380f8ede6bd230c43c46259e0
Saranya Vinjarapu <saranya@cantilever.co>
2021-12-06 04:54:19 +0530

    combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,2 +1,9 @@
 /* we will have multiple reducers and all sucj reducers will be
 combined into index.js file */
+
+import { combineReducers } from "redux";
+import { productReducer } from "./productReducer";
+
+const reducers = combineReducers({
+  allProducts: productReducer,
+});
